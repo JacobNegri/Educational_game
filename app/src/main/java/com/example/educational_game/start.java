6b7d4679822f404ce8f3ea5198fc84fccd7e0477
Jacob Negri <jc319816@ad.jcu.edu.au>
2019-05-27 22:00:50 +1000

    Initial commit

---
+++ app/src/main/java/com/example/educational_game/start.java
@@ -0,0 +1,34 @@
+package com.example.educational_game;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+
+public class start extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+
+    public void startGame(View view) {
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+
+    //Disables the return button to make sure we do not get errors and frozen screens.
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (event.getKeyCode()) {
+                case KeyEvent.KEYCODE_BACK:
+                    return true;
+            }
+        }
+
+        return super.dispatchKeyEvent(event);
+    }
+}
