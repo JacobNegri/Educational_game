c7c32b83b6bf6b168f3aeb476c3e8f346fdfa2bf
jacobnegri <jnegr3@gmail.com>
2019-05-27 23:27:14 +1000

    added in twitter function

---
+++ app/src/main/java/com/example/educational_game/Tweet.java
@@ -0,0 +1,13 @@
+/*
+package com.example.educational_game;
+
+class Tweet {
+    String author;
+    String message;
+    Tweet(String author, String message) {
+        this.author = author;
+        this.message = message;
+    }
+}
+
+*/
